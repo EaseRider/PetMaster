98bdb84c0669c8c78331c0f68308da521e031661
Alexis.Suter <alexis.suter@hsr.ch>
2016-03-03 16:18:13 +0100

    test

---
+++ src/main/java/world/master/me/App.java
@@ -0,0 +1,13 @@
+package world.master.me;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
